f66e67eaed6a7db89c7baf72e1d24ff0a8e8d8d1
root <noorshalbi@gmail.com>
2023-03-17 11:41:00 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - is the main function
+ *
+ * Return: 0 when no error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
