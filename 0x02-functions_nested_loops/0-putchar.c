4258b237d5656903b68465703090d7268170aa21
root <noorshalbi@gmail.com>
2023-03-21 10:30:18 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - the main function
  *
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-	char ch[9] = "_putchar";
-
-	for (int i = 0; i < 9; i++)
-	{
-		_putchar(ch[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
