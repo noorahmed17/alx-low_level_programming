1a88402cadb7cd9e2d421179c483c94b3795bba3
root <noorshalbi@gmail.com>
2023-03-21 11:48:07 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * _abs - checks for absolute value
+ *
+ * @n: parameter to be checked
+ *
+ * Return: Always n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
