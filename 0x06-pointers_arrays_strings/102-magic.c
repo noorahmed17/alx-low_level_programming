ccd06cf1116ceace78bf637d871d6df38bf3a639
root <noorshalbi@gmail.com>
2023-03-29 08:25:35 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check code
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	*(p + 5) = 98;
+	printf("a[2] = %d\n". a[2]);
+	return (0);
+}
