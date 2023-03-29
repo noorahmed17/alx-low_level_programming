eb9b316ec7f7127b04724edbf95ea362d6117bfb
root <noorshalbi@gmail.com>
2023-03-29 08:33:49 -0700

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,9 +14,6 @@ int main(void)
 	 /**
 	  * write your line of code here...
 	  * Remember:
-	  * - you are not allowed to use a
-	  * - you are not allowed to modify p
-	  * - only one statement
 	  * - you are not allowed to code anything else than this line of code
 	  */
 	*(p + 5) = 98;
