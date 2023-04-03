47d9abe952b509de661b585382d7b27c4b3e4fb7
root <noorshalbi@gmail.com>
2023-04-03 08:58:37 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: function parameter
+ * @needle:function parameter
+ * Return: a pointer beg of substring or @Null if it not foound.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
