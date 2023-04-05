e89b1ec2eca169235d3cbb70654686a447331c04
root <noorshalbi@gmail.com>
2023-04-05 14:55:35 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,11 +7,11 @@
  * @j: function parameter
  * Return: n
  */
-int check(cher *s1, char *s2, int i, int j)
+int check(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
-	if (s1[i] == s[j])
+	if (s1[i] == s2[j])
 		return (check(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (check(s1, s2, i, j + 1));
