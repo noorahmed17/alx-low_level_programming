5a23b73e72670a9b41c09a1a4c3c173cf1d400fc
root <noorshalbi@gmail.com>
2023-04-05 12:04:19 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
