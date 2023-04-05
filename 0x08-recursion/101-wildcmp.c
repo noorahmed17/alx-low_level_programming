312216520aba247969499ee2487b4df699820e55
root <noorshalbi@gmail.com>
2023-04-05 14:52:47 -0700

    recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * check - check if identical
+ * @s1: function parameter
+ * @s2: function parameter
+ * @i: function parameter
+ * @j: function parameter
+ * Return: n
+ */
+int check(cher *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s[j])
+		return (check(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (check(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (check(s1, s2, i + 1, j) || check(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if identical
+ * @s1: function parameter
+ * @s2: function parameter
+ * Return: n
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (check(s1, s2, 0, 0));
+}
