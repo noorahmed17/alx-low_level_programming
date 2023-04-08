7354c4d9702a9037a20765ebfdd61cf4a8938c67
root <noorshalbi@gmail.com>
2023-04-08 08:26:39 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - entry point
+ *@argc: function parameter
+ *@argv: function parameter
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
