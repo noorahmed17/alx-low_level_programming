cf422210c0e414821e8ffc4d4dc43a5c8ce3a6fe
noorahmed17 <noorshalbi@gmail.com>
2023-04-19 11:22:15 -0700

    Task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: function parameter
+ * @argv: function parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *((unsigned char*)main + i));
+	}
+	printf("\n");
+	return (0);
+}
+
+
