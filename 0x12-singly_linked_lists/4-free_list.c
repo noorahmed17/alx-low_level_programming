a229425f1479bd826c63bfbf97fb2b7cc7f7a867
noorahmed17 <noorshalbi@gmail.com>
2023-04-27 12:06:44 -0700

    Task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_list - frees a list
+ * @head: function parameter
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
