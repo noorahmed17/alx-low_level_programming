2384bd4248d847bd2a63cf62c84cf7de05605212
noorahmed17 <noorshalbi@gmail.com>
2023-04-27 12:14:33 -0700

    Task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print - print a string
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
