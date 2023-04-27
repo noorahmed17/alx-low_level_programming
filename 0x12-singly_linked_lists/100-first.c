0da90e59aaccd7f039225a6f5e7c59311b8a6dc2
noorahmed17 <noorshalbi@gmail.com>
2023-04-27 12:16:57 -0700

    Task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+void first(void) __attribute__ ((constructor));
 /**
- * print - print a string
+ * first - print a string
  * Return: void
  */
-void print(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
