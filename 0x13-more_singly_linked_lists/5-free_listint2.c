32e1e12079d6f489fb1a684afb92939bb70f771d
noorahmed17 <noorshalbi@gmail.com>
2023-04-29 11:04:18 -0700

    Task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 - frees alist
+ * @head: function parameter
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}
